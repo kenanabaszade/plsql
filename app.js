74be5a9ac69ab21e80150996925662dff7077a00
kenanabaszade <kenanabaszadeh@gmail.com>
2025-02-07 11:16:56 +0400

    changes

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 document.addEventListener("scroll", function () {
     let scrollPoint = 200;  
     let bg = document.querySelector(".bgimage");
+    let firstcontent = document.getElementById("firstcontent");
+    let secondcontent = document.getElementById("secondcontent");
   
     if (window.scrollY > scrollPoint) {
       bg.classList.add("active");
+      firstcontent.classList.add("active");
+      secondcontent.classList.add("active");
     } else {
       bg.classList.remove("active");
+      firstcontent.classList.remove("active");
+      secondcontent.classList.remove("active");
     }
   });
 
